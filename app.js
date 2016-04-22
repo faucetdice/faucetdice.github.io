623703245f17e01d9e8d56d691976266498827cc
faucetdice <faucetdice@checktest.org>
2016-04-21 18:32:05 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 var config = {
   // - Your app's id on moneypot.com
-  app_id: '1088',                             // <----------------------------- EDIT ME!
+  app_id: 1088,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
   app_name: 'Faucet Dice',
   // - For your faucet to work, you must register your site at Recaptcha
